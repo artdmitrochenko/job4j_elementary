5d298f5a4cf9860c6f91dec9846f981a5e1376e7
Arthur <barcarti.ru@gmail.com>
2022-12-13 00:56:54 +0300

    9.2. Математические операторы и составные операторы присваивания [#504905]

---
+++ src/main/java/ru/job4j/condition/StringRunner.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        String i = "I like Java!";
+        System.out.println(i);
+        i += " But I am a newbie. ";
+        int year = 2022;
+        i += year;
+        System.out.println(i);
+    }
+}
